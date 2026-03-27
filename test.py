6ea1241672b2763180b0f8072e3f5aa41f85ef4c
Navi <singhnav0309@gmail.com>
2026-03-27 23:23:46 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,30 @@
+import psycopg2
+def table():
+     conn = psycopg2.connect(dbname='postgres', user='postgres', password='1234', host='localhost', port='6304')
+
+     cursor = conn.cursor()
+     cursor.execute('''create table employees(Name Text, ID int, Age int)''')
+     print('Table created successfully')
+
+     conn.commit()
+     conn.close()
+
+def data():
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='1234', host='localhost', port='6304')
+
+    cursor = conn.cursor()
+
+    name = input('Enter name: ')
+    id = input('Enter ID: ')
+    age = input('Enter age: ') 
+
+    query = '''insert into employees(Name, ID, Age) values(%s, %s, %s);'''
+    cursor.execute(query, (name, id, age))
+
+    print('Data inserted successfully')
+
+    conn.commit()
+    conn.close()
+
+data()
+
